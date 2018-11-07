ce1a32e4f1a991bdbd690d6fec644eaf04a45c25
Manny <egalli64@gmail.com>
2018-11-07 12:20:38 +0100

    Adding checks on data passed in to find()

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -4,22 +4,28 @@ public class NumFinder {
     private int smallest = Integer.MAX_VALUE;
     private int largest = Integer.MIN_VALUE;
 
-    public void find(int[] nums) {
-        for(int n : nums) {
+    public void find(int[] numbers) {
+        // reset results
+        smallest = Integer.MAX_VALUE;
+        largest = Integer.MIN_VALUE;
 
-            if(n < smallest)
+        if (numbers == null || numbers.length == 0) {
+            return;
+        }
+
+        for (int n : numbers) {
+            if (n < smallest)
                 smallest = n;
-            else if (n > largest)
+            if (n > largest)
                 largest = n;
-
         }
     }
 
-    public int getSmallest () {
+    public int getSmallest() {
         return smallest;
     }
 
-    public int getLargest () {
+    public int getLargest() {
         return largest;
     }
 }
--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -2,15 +2,21 @@ package tudelft.numfinder;
 
 public class NumFinderMain {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         NumFinder nf = new NumFinder();
 
         // this works
-        // nf.find(new int[] {4, 25, 7, 9});
+        nf.find(new int[]{4, 25, 7, 9});
+        System.out.println(nf.getLargest());
+        System.out.println(nf.getSmallest());
 
-        // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        // this crashed
+        nf.find(new int[]{4, 3, 2, 1});
+        System.out.println(nf.getLargest());
+        System.out.println(nf.getSmallest());
 
+        // this should probably throw an exception
+        nf.find(null);
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
     }

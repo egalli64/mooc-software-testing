d7874ca6d15d63cf683d428ba55a9406225f024d
Manny <egalli64@gmail.com>
2018-11-08 14:45:25 +0100

    Petty code changes following the lesson

--- src/test/java/tudelft/leapyear/LeapYearTest.java
+++ src/test/java/tudelft/leapyear/LeapYearTest.java
@@ -1,31 +1,29 @@
 package tudelft.leapyear;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LeapYearTest {
+
     @Test
     public void leapYearsThatAreNonCenturialYears() {
-        LeapYear ly = new LeapYear();
-        boolean result = ly.isLeapYear(2016);
+        boolean result = LeapYear.isLeapYear(2016);
         Assertions.assertTrue(result);
     }
 
     @Test
     public void leapCenturialYears() {
-        LeapYear ly = new LeapYear();
-        Assertions.assertTrue(ly.isLeapYear(2000));
+        Assertions.assertTrue(LeapYear.isLeapYear(2000));
     }
 
     @Test
     public void nonLeapCenturialYears() {
-        LeapYear ly = new LeapYear();
-        Assertions.assertFalse(ly.isLeapYear(1500));
+        Assertions.assertFalse(LeapYear.isLeapYear(1500));
     }
 
     @Test
     public void nonLeapYears() {
-        LeapYear ly = new LeapYear();
-        Assertions.assertFalse(ly.isLeapYear(2017));
+        Assertions.assertFalse(LeapYear.isLeapYear(2017));
     }
 }

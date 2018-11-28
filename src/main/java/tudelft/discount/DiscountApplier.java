4c7789a3561f9d0b1f49b24df9a993ab60723cfe
Manny <egalli64@gmail.com>
2018-11-28 10:14:25 +0100

    Topic 4 Practical exercise #2

--- src/main/java/tudelft/discount/DiscountApplier.java
+++ src/main/java/tudelft/discount/DiscountApplier.java
@@ -4,20 +4,21 @@ public class DiscountApplier {
 
     private ProductDao dao;
 
-    public DiscountApplier (ProductDao dao) {
+    public DiscountApplier(ProductDao dao) {
         this.dao = dao;
     }
 
     public void setNewPrices() {
 
-        for(Product product : dao.all()) {
-            if(product.getCategory().equals("BUSINESS")) {
-                product.setPrice(product.getPrice() * 0.9);
-            }
-            if(product.getCategory().equals("HOME")) {
-                product.setPrice(product.getPrice() * 1.1);
+        for (Product product : dao.all()) {
+            switch(product.getCategory()) {
+                case "BUSINESS":
+                    product.setPrice(product.getPrice() * 1.1);
+                    break;
+                case "HOME":
+                    product.setPrice(product.getPrice() * .9);
+                    break;
             }
         }
-
     }
 }
--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,26 @@
 package tudelft.discount;
 
-public class DiscountApplierTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.Arrays;
+import java.util.List;
+
+class DiscountApplierTest {
+    @Test
+    void plain() {
+        Product home = new Product("home", 100, "HOME");
+        Product biz = new Product("biz", 100, "BUSINESS");
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        List<Product> results = Arrays.asList(home, biz);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        DiscountApplier disc = new DiscountApplier(dao);
+        disc.setNewPrices();
+
+        Assertions.assertEquals(90.0, home.getPrice(), 0.001);
+        Assertions.assertEquals(110.0, biz.getPrice(), 0.001);
+    }
 }
